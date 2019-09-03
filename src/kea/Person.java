edf817aad69e34296ac7f72553251d891d1dfcfc
Nikolaj <43247719+Nikolaj93@users.noreply.github.com>
2019-09-03 14:59:17 +0200

    Create Person.java

---
+++ src/kea/Person.java
@@ -0,0 +1,30 @@
+package kea;
+
+public class Person {
+
+    private String name;
+    private long cpr;
+
+    public Person(String name, long cpr) {
+        this.name = name;
+        this.cpr = cpr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(long cpr) {
+        // identificer at det er her der skal være kode
+        this.cpr = cpr;
+
+    }
+}
